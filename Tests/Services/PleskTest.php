10f92054ef09dfdbfdda8c57c162d20e19a6a9f8
zivko <zivko.sudarski@gmail.com>
2014-08-25 09:53:07 +1200

    Add phpunit test

---
+++ Tests/Services/PleskTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the GeneralDigital\PleskBundle
+*
+* (c) Zivko Sudarski <zivko@generaldigital.co.nz>
+*
+* This source file is subject to the MIT license that is bundled
+* with this source code in the file LICENSE.
+*/
+
+/**
+ * Test Plesk Service class
+ *
+ * @author Zivko Sudarski <zivko@generaldigital.co.nz>
+ */
+namespace GeneralDigital\PleskBundle\Tests;
+
+use GeneralDigital\PleskBundle\Services\Plesk;
+
+class PleskTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test Plesk constructor
+     *
+     * @covers GeneralDigital\PleskBundle\Services\Plesk::__construct
+     */
+    public function testPleskConstructor()
+    {
+        $plesk = new Plesk('host.com', 'zivko', 'sudars123');
+
+        $this->assertEquals('host.com', $plesk->getHost());
+        $this->assertEquals('zivko', $plesk->getUser());
+        $this->assertEquals('sudars123', $plesk->getPassword());
+    }
+}
