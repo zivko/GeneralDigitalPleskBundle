e2e5aaa35890d59b68fce2217f3f4e4eeaee0970
Zivko Sudarski <zivko.sudarski@gmail.com>
2014-08-25 12:58:39 +1200

    Update GeneralDigitalPleskBundle.php

--- GeneralDigitalPleskBundle.php
+++ GeneralDigitalPleskBundle.php
@@ -1,9 +1,23 @@
 <?php
 
+/*
+ * This file is part of the GeneralDigital\PleskBundle
+*
+* (c) Zivko Sudarski <zivko@generaldigital.co.nz>
+*
+* This source file is subject to the MIT license that is bundled
+* with this source code in the file LICENSE.
+*/
+
 namespace GeneralDigital\PleskBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * GeneralDigitalPleskBundle
+ *
+ * @author Zivko Sudarski <zivko@generaldigital.co.nz>
+ */
 class GeneralDigitalPleskBundle extends Bundle
 {
 }
